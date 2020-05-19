a8b6e40ff731008656c4817b13359a34f313117c
jessc1 <mmariajessica@gmail.com>
2020-05-19 14:59:59 -0300

    another test

---
+++ recipe5_runner.py
@@ -0,0 +1,10 @@
+from recipe5 import *
+import unittest
+if __name__ == "__main__":
+       
+    suite1 = unittest.TestLoader().loadTestsFromTestCase( \
+                RomanNumeralConverterTest)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase( \
+                RomanNumeralComboTest)
+    suite = unittest.TestSuite([suite1, suite2])
+    unittest.TextTestRunner(verbosity=2).run(suite)
